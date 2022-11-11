6404fbb73dedeccfb8651ead8c7f4ffb23a8303f
dread557 <dadaolawunmi09@gmail.com>
2022-11-11 17:00:31 +0100

    Feat: set up routes
    Initial commits and page routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Communities, Home, Nfts, Places } from './pages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/places' element={<Places />} />
+        <Route exact path='/communities' element={<Communities />} />
+        <Route exact path='/nfts' element={<Nfts />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home'
+export { default as Places } from './Places'
+export { default as Nfts } from './Nfts'
+export { default as Communities } from './Communities'
